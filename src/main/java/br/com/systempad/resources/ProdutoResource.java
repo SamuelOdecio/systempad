db61780a9bd674a036dcbb6bcb40b2aaab6254b8
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-08-19 01:31:23 -0300

    Implementação de POST, PUT, DELETE - Classe Produto

--- src/main/java/br/com/systempad/resources/ProdutoResource.java
+++ src/main/java/br/com/systempad/resources/ProdutoResource.java
@@ -1,13 +1,19 @@
 package br.com.systempad.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.systempad.dto.ProdutoDTO;
 import br.com.systempad.services.ProdutoService;
@@ -29,5 +35,30 @@ public class ProdutoResource {
 	public ResponseEntity<ProdutoDTO> findById(@PathVariable Long id){
 		ProdutoDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProdutoDTO> insert(@RequestBody ProdutoDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(dto.getId_produto())
+					.toUri();
+		return ResponseEntity.created(uri).body(null);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProdutoDTO> update(
+			@PathVariable Long id,
+			@RequestBody ProdutoDTO dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/br/com/systempad/services/ProdutoService.java
+++ src/main/java/br/com/systempad/services/ProdutoService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.systempad.dto.ProdutoDTO;
 import br.com.systempad.entities.Produto;
 import br.com.systempad.repositories.ProdutoRepository;
+import br.com.systempad.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -31,5 +34,47 @@ public class ProdutoService {
 		Optional<Produto> obj = repository.findById(id);
 		Produto entity = obj.get();
 		return new ProdutoDTO(entity);		
+	}
+	
+	@Transactional
+	public ProdutoDTO insert(ProdutoDTO dto) {
+		Produto entity = new Produto();
+		entity.setNome(dto.getNome());
+		entity.setDescricao(dto.getDescricao());
+		entity.setPreco(dto.getPreco());
+		
+
+		entity = repository.save(entity);
+
+		return new ProdutoDTO(entity);
+	}
+
+	@Transactional
+	public ProdutoDTO update(Long id, ProdutoDTO dto) {
+
+		try {
+			Produto entity = repository.getReferenceById(id);
+
+			entity.setNome(dto.getNome());
+			entity.setDescricao(dto.getDescricao());
+			entity.setPreco(dto.getPreco());
+			
+
+			entity = repository.save(entity);
+
+			return new ProdutoDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(
+					"O recurso com o ID "+id+" não foi localizado");
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(
+					"O recurso com o ID "+id+" não foi localizado");
+		}
 	}
 }
