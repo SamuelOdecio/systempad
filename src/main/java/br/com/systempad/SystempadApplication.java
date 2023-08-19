8f9813a00579e7ec2f99c92455228fcfc59eec61
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-08-18 23:14:38 -0300

    First Comit

---
+++ src/main/java/br/com/systempad/SystempadApplication.java
@@ -0,0 +1,13 @@
+package br.com.systempad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystempadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystempadApplication.class, args);
+	}
+
+}
