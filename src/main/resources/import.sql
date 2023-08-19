63988c6ec14c2bd02fe31def7697230d6f072ad2
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-08-19 00:59:10 -0300

    Preparação do ambiente para execução do BDH2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_PRODUTO (nome, descricao, preco) VALUES ('Pao Frances', 'Pãozinho quentinho', 0.5);
+INSERT INTO TB_PRODUTO (nome, descricao, preco) VALUES ('Manteira', 'Qually Com Sal', '7.5');
+INSERT INTO TB_PRODUTO (nome, descricao, preco) VALUES ('Mortandela Defumada', 'Mortandela Defumada Fatiada', '0.5');
+INSERT INTO TB_PRODUTO (nome, descricao, preco) VALUES ('Queijo Mussarela', 'Queijo Mussarela Fatiado', '0.5');
