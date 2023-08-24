7c36a8e5464797bb08c7689a862c7bef7fd27ce2
Samuel Odecio <108842146+SamuelOdecio@users.noreply.github.com>
2023-08-24 13:39:46 -0400

    Exclusao da Classe GERENTE e correção do import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,4 +5,3 @@ INSERT INTO TB_PRODUTO (nome, descricao, preco) VALUES ('Queijo Mussarela', 'Que
 INSERT INTO TB_CLIENTE (nome, cpf, telefone, email, user_name, senha, endereco) VALUES ('Samuel','111.222.070-99','67 99999-0000','samuel@gmail.com','samuel190','123456', 'AV. Dourados, 111');
 INSERT INTO TB_ATENDENTE (nome, cpf, telefone, email, user_name, senha) VALUES ('Samuel','111.222.070-99','67 99999-0000','samuel@gmail.com','samuel190','123456');
 INSERT INTO TB_ATENDENTE (nome, cpf, telefone, email, user_name, senha) VALUES  ('Gabriel','111.333.060-99','67 99999-0011','gabriel@gmail.com','gabriel190','000000');
-INSERT INTO TB_GERENTE (nome, cpf, telefone, email, user_name, senha) VALUES  ('Marcos','111.444.050-99','67 99999-0022','marcos@gmail.com','marcos190','162534');
