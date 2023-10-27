a6a5e84212db5a8e4243f836c8a2228a005e24fe
Gabriel Suldine <gabriel.suldinne@gmail.com>
2023-10-26 22:16:21 -0400

    CORS config

--- src/main/java/br/com/systempad/SystempadApplication.java
+++ src/main/java/br/com/systempad/SystempadApplication.java
@@ -1,11 +1,19 @@
 package br.com.systempad;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class SystempadApplication {
+public class SystempadApplication  implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SystempadApplication.class, args);
 	}
